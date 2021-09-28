cb08749ce23ea66703f961ff5f24936fff151839
João Pedro Toledo <31158250+JoaoPedroToledo@users.noreply.github.com>
2021-09-28 18:11:27 -0300

    v2

--- ProjetoDjango/djangoS/models.py
+++ ProjetoDjango/djangoS/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django import forms
 
 class ClienteModel(models.Model):
     id_cliente=models.IntegerField(primary_key=True)
@@ -18,3 +19,29 @@ class MaterialModel(models.Model):
     material=models.CharField(max_length=100)
     class Meta:
          db_table = "materia_prima"
+
+
+class TratamentoSuperficialModel(models.Model):
+    id_superf=models.IntegerField(primary_key=True)
+    nome_tratamento=models.CharField(max_length=100)
+    class Meta:
+         db_table = "tratamento_superficial"
+
+class TratamentoTermicoModel(models.Model):
+    id_termico=models.IntegerField(primary_key=True)
+    nome_trat_termico=models.CharField(max_length=100)
+    class Meta:
+         db_table = "tratamento_termico"
+
+
+class AnaliseCriticaModel(models.Model):
+    numero_cotacao=models.CharField(max_length=100)
+    cliente=models.CharField(max_length=50)
+    contato=models.CharField(max_length=50)
+    materia_prima=models.CharField(max_length=50)
+    pn=models.CharField(max_length=50)
+    rev=models.CharField(max_length=50)
+    data=models.DateTimeField()
+    assinatura=models.CharField(max_length=50)
+    class Meta:
+        db_table = "AnalisedaCotacao"
--- ProjetoDjango/djangoS/views.py
+++ ProjetoDjango/djangoS/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render             
-from djangoS.models import ClienteModel, EmpresaModel, MaterialModel
+from djangoS.models import ClienteModel, EmpresaModel, MaterialModel, TratamentoSuperficialModel, TratamentoTermicoModel
 
 def djangoSCliente(request):
     clienteobj=ClienteModel.objects.all()
     empresaobj=EmpresaModel.objects.all()
-    materialobj=MaterialModel.objects.all()    
-    return render(request, 'index.html', {"Clientedata": clienteobj, "Empresadata": empresaobj, "Materialdata":materialobj})
+    materialobj=MaterialModel.objects.all()
+    tratamentosuperficialobj=TratamentoSuperficialModel.objects.all()
+    tratamentotermicoobj=TratamentoTermicoModel.objects.all()
+    return render(request, 'index.html', {"Clientedata": clienteobj, "Empresadata": empresaobj, "Materialdata":materialobj, "TratamentoSuperficialdata":tratamentosuperficialobj, "TratamentoTermicodata":tratamentotermicoobj})
